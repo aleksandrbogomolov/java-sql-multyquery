16a801a4084134e24659b82c0cbb20c408e77c09
bogomolov_av <bogomolov_av@magnit.ru>
2017-12-22 12:17:10 +0300

    Made getting user/password from console. Added some new query. Deleted old tnsnames.

--- resources/old_query.sql
+++ resources/old_query.sql
@@ -1,4 +1,4 @@
-SELECT status, count(*) FROM rpl_user.fde_vetrequest_queue WHERE status IN (1, 2, 3) and enqueue_time <= sysdate - 1/48 and enqueue_time >= sysdate - 2 GROUP BY status
+SELECT status, count(*) FROM rpl_user.fde_vetrequest_queue WHERE status IN (1, 2, 3) and enqueue_time <= sysdate - 1/24 and enqueue_time >= sysdate - 2 GROUP BY status
 
 SELECT request_type, status, count(*) FROM rpl_user.fde_vetrequest_queue
 WHERE status IN (1, 2, 3)
@@ -18,3 +18,24 @@ select null status, null date_time, sum(requests_cnt) request_count, null min_id
 from bad_requests
 union all
 select * from bad_requests
+
+select max(code || ' ' || fullname) from warehouse where id_controwner is not null;
+with bad_requests as
+(select status, trunc(cast(create_date as timestamp), 'HH') date_hour, count(id_queue) requests_cnt,
+  min(id_queue) min_request_id, max(id_queue) max_request_id
+  from rpl_user.fde_vetrequest_queue
+  where enqueue_time >= sysdate - 2 and enqueue_time <= sysdate - 1 / 48 and status in (0, 1, 2, 3)
+  group by status, trunc(cast(create_date as timestamp), 'HH')
+  order by status, trunc(cast(create_date as timestamp), 'HH'))
+select null status, null date_time, sum(requests_cnt) request_count, null min_id_queue, null max_id_queue
+from bad_requests
+union all
+select * from bad_requests
+
+select count(*) from rpl_user.fde_vetrequest_queue
+where status in (1, 2, 3)
+and enqueue_time >= trunc(sysdate)
+
+SELECT status, count(*) FROM rpl_user.fde_vetrequest_queue WHERE status IN (1) and enqueue_time >= trunc(sysdate) GROUP BY status
+
+select count(*) from rpl_user.fde_vetrequest_out_qt
--- src/main/java/com/aleksandrbogomolov/ora/OraJavaApplication.java
+++ src/main/java/com/aleksandrbogomolov/ora/OraJavaApplication.java
@@ -4,22 +4,43 @@ import static com.aleksandrbogomolov.ora.helper.FileHelper.parseTns;
 import static com.aleksandrbogomolov.ora.helper.FileHelper.readQuery;
 
 import com.aleksandrbogomolov.ora.helper.DbHelper;
+import java.io.BufferedInputStream;
+import java.io.Console;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
+import java.util.Scanner;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class OraJavaApplication {
 
   public static void main(String[] args) throws SQLException {
+    Console console = System.console();
+    String user = null;
+    char[] password = new char[0];
+    if (console != null) {
+      user = console.readLine("Enter user name: ");
+      password = console.readPassword("Enter password: ");
+    }
     OracleDataSource dataSource = new OracleDataSource();
     String[] sqls = readQuery();
+    System.out.println(
+        LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+            + "\n");
+    String finalUser = user;
+    char[] finalPassword = password;
     parseTns().forEach((key, value) -> {
       for (String sql : sqls) {
         dataSource.setURL("jdbc:oracle:thin:@" + value);
-        try (Connection connection = dataSource.getConnection("bogomolov_av", "")) {
+        try (Connection connection = dataSource.getConnection(finalUser,
+            String.valueOf(finalPassword))) {
           PreparedStatement statement = connection.prepareStatement(sql);
           ResultSet resultSet = statement.executeQuery();
           ResultSetMetaData metaData = resultSet.getMetaData();
