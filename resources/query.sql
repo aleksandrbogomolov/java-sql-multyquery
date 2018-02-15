43c8be87604c1ebade76cae6aa382e6864ceb4e0
bogomolov_av <bogomolov_av@magnit.ru>
2018-02-15 14:58:41 +0300

    Github initial commit

--- resources/query.sql
+++ resources/query.sql
@@ -1,9 +1,7 @@
 select max(code || ' ' || fullname) from warehouse where id_controwner is not null;
-SELECT
-  status,
-  count(status)
-FROM rpl_user.fde_vetrequest_queue
-WHERE status IN (0, 1, 2, 3, 4, 5)
-      AND create_date > sysdate - 7
-GROUP BY status
-ORDER BY status
+select status, trunc(cast(create_date as timestamp), 'HH') date_hour, count(id_queue) requests_cnt,
+          trunc(cast(lastdate as timestamp), 'HH') lastdate, min(id_queue) min_request_id, max(id_queue) max_request_id
+   from rpl_user.fde_vetrequest_queue
+ where enqueue_time >= sysdate - 1 and enqueue_time <= sysdate - 1/24 and status in (0, 1, 2, 3)
+ group by status, trunc(cast(create_date as timestamp), 'HH'), trunc(cast(lastdate as timestamp), 'HH')
+ order by status, trunc(cast(create_date as timestamp), 'HH')
